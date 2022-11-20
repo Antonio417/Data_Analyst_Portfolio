cf7e69106f137568f67509ebaa629fb944d427f4
Antonio <airantoniofernando@gmail.com>
2022-11-20 14:06:58 +0700

    Added More Queries

--- SQL/SQL Aggregation Queries.sql
+++ SQL/SQL Aggregation Queries.sql
@@ -235,4 +235,75 @@ GROUP BY ord_month
 ORDER BY total_spent DESC
 LIMIT 1;
 
+/* Write a query to display for each order, the account ID, total amount of the order, 
+and the level of the order - ‘Large’ or ’Small’ - depending on if the order is $3000 or more, or 
+smaller than $3000. */
+SELECT account_id, total_amt_usd,
+CASE WHEN total_amt_usd > 3000 THEN 'Large'
+ELSE 'Small' END AS order_level
+FROM orders;
+
+/* Write a query to display the number of orders in each of three categories, 
+based on the total number of items in each order. 
+The three categories are: 'At Least 2000', 'Between 1000 and 2000' and 'Less than 1000'. */
+SELECT CASE WHEN total >= 2000 THEN 'At Least 2000'
+WHEN total >= 1000 AND total < 2000 THEN 'Between 1000 and 2000'
+ELSE 'Less than 1000' END AS order_category,
+COUNT(*) AS order_count
+FROM orders
+GROUP BY 1;
+
+/* We would like to understand 3 different levels of customers based on the amount associated with their purchases. 
+Provide a table that includes the level associated with each account. */
+SELECT a.name, SUM(o.total_amt_usd) as total_sales,
+CASE WHEN SUM(o.total_amt_usd) > 200000 THEN 'Top'
+WHEN SUM(o.total_amt_usd) >= 100000 AND SUM(o.total_amt_usd) < 200000 THEN 'Middle'
+ELSE 'Low' END AS lifetime_value
+FROM orders o
+JOIN accounts a
+ON a.id = o.account_id
+GROUP BY a.name
+ORDER BY total_sales DESC;
+
+/* We would now like to perform a similar calculation to the first, 
+but we want to obtain the total amount spent by customers only in 2016 and 2017. */
+SELECT a.name, SUM(o.total_amt_usd) as total_sales,
+CASE WHEN SUM(o.total_amt_usd) > 200000 THEN 'Top'
+WHEN SUM(o.total_amt_usd) >= 100000 AND SUM(o.total_amt_usd) < 200000 THEN 'Middle'
+ELSE 'Low' END AS lifetime_value
+FROM orders o
+JOIN accounts a
+ON a.id = o.account_id
+WHERE o.occurred_at > '2015-12-31' 
+GROUP BY a.name
+ORDER BY total_sales DESC;
+
+/* We would like to identify top performing sales reps, 
+which are sales reps associated with more than 200 orders. */
+SELECT s.name, COUNT(*) num_ords,
+     CASE WHEN COUNT(*) > 200 THEN 'top'
+     ELSE 'not' END AS sales_rep_level
+FROM orders o
+JOIN accounts a
+ON o.account_id = a.id 
+JOIN sales_reps s
+ON s.id = a.sales_rep_id
+GROUP BY s.name
+ORDER BY num_ords DESC;
+
+/* The previous didn't account for the middle, nor the dollar amount associated with the sales. 
+Management decides they want to see these characteristics represented as well. Create a table with the sales rep name,
+the total number of orders, total sales across all orders, and a column with top, middle, 
+or low depending on this criteria.  */
+SELECT s.name, COUNT(*), SUM(o.total_amt_usd) total_spent, 
+CASE WHEN COUNT(*) > 200 OR SUM(o.total_amt_usd) > 750000 THEN 'top'
+WHEN COUNT(*) > 150 OR SUM(o.total_amt_usd) > 500000 THEN 'middle'
+ELSE 'low' END AS sales_rep_level
+FROM orders o
+JOIN accounts a
+ON o.account_id = a.id 
+JOIN sales_reps s
+ON s.id = a.sales_rep_id
+GROUP BY s.name
+ORDER BY total_spent DESC;
 
